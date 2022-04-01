6158b4eef1cfd9a0628787968ed655c54dcaacd6
bandela.vamshi1 <vamshibandela777@gmail.com>
2022-04-01 12:35:50 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/timecalculator"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/timecalculator/index.html"));
-});
+
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/timecalculator"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/timecalculator" })
+);
 
 app.listen(process.env.PORT || 8080);
